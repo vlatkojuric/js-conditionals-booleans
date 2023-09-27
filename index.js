eef6c3391a95795eabb25d54def64090deacf73c
Vlatko Juric <juricv78gmail.com>
2023-09-27 20:55:02 +0200

    got to the 3rd task in challenge number one,must finish

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ console.clear();
 // Part 1: Password
 const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
-const receivedPassword = "password1234";
+const receivedPassword = "h4x0r1337";
+
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
 
 // Part 2: Even / Odd
 const number = 6;
 
+if (number % 2 === 0) {
+  console.log("the number is even");
+} else {
+  console.log("the number is odd");
+}
+
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 100;
+
+if (numberOfHotdogs <= 5) {
+  console.log("2 euro per hot dog");
+} else if (numberOfHotdogs > 5 < 100) {
+  console.log("1.50 euro per hot dog");
+}
 
 // Part 4: Daytime
 const currentHour = 12;
